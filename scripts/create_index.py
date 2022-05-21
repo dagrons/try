b4cec46887c231834684af15d4294c0e71281d9d
dagrons <heyuehuii@126.com>
2022-05-21 16:31:13 +0800

    add hashset to remove duplicate

--- scripts/create_index.py
+++ scripts/create_index.py
@@ -11,6 +11,7 @@ with open("index.csv", "w") as csvfile:
     index_writer = csv.DictWriter(csvfile, fieldnames=["filename", "sha256", "md5"])
     index_writer.writeheader()
     fcnt = 0
+    hashset = set()
     for root, _, files in os.walk(sys.argv[1]):
         for f in files:
             fcnt += 1
@@ -30,4 +31,7 @@ with open("index.csv", "w") as csvfile:
                         md5_hash.update(block)
                 sha256_s = sha256_hash.hexdigest()
                 md5_s = md5_hash.hexdigest()
+                if sha256_s in hashset:
+                    continue
+                hashset.add(sha256_s)
                 index_writer.writerow({"filename": fpath, "sha256": sha256_s, "md5": md5_s})
