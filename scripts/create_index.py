bc9c3a1e52a2e56e0aad776b984258036ce4fe50
dagrons <heyuehuii@126.com>
2022-05-21 18:05:30 +0800

    add skip-dup-file option and argparser

--- scripts/create_index.py
+++ scripts/create_index.py
@@ -6,13 +6,31 @@ import sys
 import lief
 import csv
 import tqdm
+import argparse
 
-with open("index.csv", "w") as csvfile:
+# parse the arguments
+argp = argparse.ArgumentParser()
+argp.add_argument("folder", type=str, default=".", help = "target folder")
+argp.add_argument("--skip-dup-file", action='store_true', help = "skip duplicate files")
+args = argp.parse_args()
+
+
+hashset = set()
+fpathset = set()
+
+# rebuild index
+with open("index.csv", "r") as csvfile:
+    index_reader = csv.DictReader(csvfile)
+    for row in index_reader:
+        fpathset.add(row['filename'])
+
+# append index
+with open("index.csv", "a") as csvfile:
     index_writer = csv.DictWriter(csvfile, fieldnames=["filename", "sha256", "md5"])
-    index_writer.writeheader()
+    if len(fpathset) == 0:
+        index_writer.writeheader()
     fcnt = 0
-    hashset = set()
-    for root, _, files in os.walk(sys.argv[1]):
+    for root, _, files in os.walk(args.folder):
         for f in files:
             fcnt += 1
     print("total files: %d" % (fcnt))
@@ -22,6 +40,8 @@ with open("index.csv", "w") as csvfile:
             tq.update(1)
             fpath = os.path.join(root, file)
             fpath = os.path.abspath(fpath)
+            if fpath in fpathset and args.skip_dup_file:
+                continue
             if lief.is_pe(fpath):
                 sha256_hash = hashlib.sha256()
                 md5_hash = hashlib.md5()
@@ -34,4 +54,5 @@ with open("index.csv", "w") as csvfile:
                 if sha256_s in hashset:
                     continue
                 hashset.add(sha256_s)
+                fpathset.add(fpath)
                 index_writer.writerow({"filename": fpath, "sha256": sha256_s, "md5": md5_s})
