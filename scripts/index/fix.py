f18e48df4c9b327aa8d8dbed389f8afa69f8eb7f
dagrons <heyuehuii@126.com>
2022-05-23 11:42:04 +0800

    fix file open mode

--- scripts/index/fix.py
+++ scripts/index/fix.py
@@ -20,12 +20,12 @@ def main():
     args = argp.parse_args()
 
     src_set = set()
-    with open(args.source_file) as f:
+    with open(args.source_file, 'r') as f:
         csv_reader = csv.DictReader(f, fieldnames=["filename", "sha256", "md5"])
         for row in csv_reader:
             src_set.add(Record(row['filename'], row['sha256'], row['md5']))
 
-    with open(args.target_file) as f:
+    with open(args.target_file, 'w+') as f:
         csv_writer = csv.DictWriter(f, fieldnames=["filename", "sha256", "md5"])
         csv_writer.writeheader()
         for record in src_set:
