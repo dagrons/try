b9f0e25872025b3ff34c83a15dec20d71e58efe2
dagrons <heyuehuii@126.com>
2022-05-24 17:17:42 +0800

    add support for multiple files merge

--- scripts/index/merge.py
+++ scripts/index/merge.py
@@ -17,33 +17,22 @@ class Record:
 
 def main():
     argp = argparse.ArgumentParser()
-    argp.add_argument("a_index")
-    argp.add_argument("b_index")
-    argp.add_argument("c_index")
+    argp.add_argument("infiles", nargs='+', required=True)
+    argp.add_argument("ofile")
     args = argp.parse_args()
 
-    a_index_path = args.a_index
-    b_index_path = args.b_index
-    c_index_path = args.c_index
+    in_data = set()
+    for infile in args.infiles:
+        with open(infile, 'r') as f:
+            in_reader = csv.DictReader(f, fieldnames=['filename', 'sha256', 'md5'])
+            for row in in_reader:
+                in_data.add(Record(row['filename'], row['sha256'], row['md5']))
 
-    a_data = set()
-    with open(a_index_path, 'r') as f:
-        a_reader = csv.DictReader(f, fieldnames=['filename', 'sha256', 'md5'])
-        for row in a_reader:
-            a_data.add(Record(row['filename'], row['sha256'], row['md5']))
-
-    b_data = set()
-    with open(b_index_path, 'r') as f:
-        b_reader = csv.DictReader(f, fieldnames=['filename', 'sha256', 'md5'])
-        for row in b_reader:
-            b_data.add(Record(row['filename'], row['sha256'], row['md5']))
-
-    c_data = a_data.union(b_data)
-    with open(c_index_path, 'w+') as f:
-        c_writer = csv.DictWriter(f, fieldnames=["filename", "sha256", "md5"])
-        c_writer.writeheader()
-        for r in c_data:
-            c_writer.writerow({'filename': r.filename, 'sha256': r.sha256, 'md5': r.md5})
+    with open(args.ofile, 'w+') as f:
+        o_writer = csv.DictWriter(f, fieldnames=["filename", "sha256", "md5"])
+        o_writer.writeheader()
+        for r in in_data:
+            o_writer.writerow({'filename': r.filename, 'sha256': r.sha256, 'md5': r.md5})
 
 
 if __name__ == "__main__":
