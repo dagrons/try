53f5b8d6c71744c86c0c6a672fb834b68fee1067
dagrons <heyuehuii@126.com>
2022-05-25 02:46:22 +0800

    replace builder pattern with named parameters

--- fetch/fetch.py
+++ fetch/fetch.py
@@ -2,30 +2,25 @@ import csv
 import tempfile
 
 import filebrowser
+from typing import List
 
 
 class Fetcher:
-    def __init__(self):
+    def __init__(self, indexes: List[str] = [], downloader: filebrowser.FileBrowserClient = None):
         self.sha256_to_fpath = {}
         self.md5_to_fpath = {}
-        self.downloader = None
-        self.cache = {}
-
-    def with_downloader(self, downloader: filebrowser.FileBrowserClient):
         self.downloader = downloader
-        return self
+        self.cache = {}
+        for fpath in indexes:
+            with open(fpath, 'r') as f:
+                csv_reader = csv.DictReader(f, fieldnames=['filename', 'sha256', 'md5'])
+                for line in csv_reader:
+                    if line['filename'] == 'filename':
+                        continue
+                    self.sha256_to_fpath[line['sha256']] = line['filename']
+                    self.md5_to_fpath[line['md5']] = line['filename']
 
-    def with_index(self, fpath):
-        with open(fpath, 'r') as f:
-            csv_reader = csv.DictReader(f, fieldnames=['filename', 'sha256', 'md5'])
-            for line in csv_reader:
-                if line['filename'] == 'filename':
-                    continue
-                self.sha256_to_fpath[line['sha256']] = line['filename']
-                self.md5_to_fpath[line['md5']] = line['filename']
-        return self
-
-    def fetch(self, hash, save_path=None):
+    def fetch(self, hash: str, save_path: str = None):
         if hash in self.cache:
             return open(self.cache[hash], 'rb')
         if save_path is None:
@@ -38,12 +33,13 @@ class Fetcher:
             raise ValueError
         self.downloader.download_auth_file(filename, save_path)
         self.cache[hash] = save_path
-        return open(save_path, 'rb')
+        return open(save_path, 'rb').read()
 
 
 if __name__ == '__main__':
-    downloader = filebrowser.FileBrowserClient().with_host(host="10.112.108.112", port="8081", username="admin",
-                                                           password="daxiahyh")
-    fetcher = Fetcher().with_index("../all.benign.csv").with_downloader(downloader)
-    reader = fetcher.fetch("0a8deb24eef193e13c691190758c349776eab1cd65fba7b5dae77c7ee9fcc906")
-    print(reader.read())
+    downloader = filebrowser.FileBrowserClient(
+        hosts=[filebrowser.HostInfo("10.112.108.112", "8081", "admin", "daxiahyh")])
+    fetcher = Fetcher(indexes=["../all.benign.csv", "../all.malware.csv"],
+                      downloader=downloader)
+    bytez = fetcher.fetch("0a8deb24eef193e13c691190758c349776eab1cd65fba7b5dae77c7ee9fcc906")
+    print(hex(len(bytez)))
