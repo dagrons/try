dfea54d768d8f881eb2ce861b2f11c204d3aeef3
dagrons <heyuehuii@126.com>
2022-05-26 13:57:44 +0800

    add .vscode

--- manipulate/manipulater.py
+++ manipulate/manipulater.py
@@ -1,26 +1,22 @@
-import filebrowser
 import lief
 from gym_malware.envs.controls import manipulate2
 
 from fetch import fetch
-
+import filebrowser
 
 class MyManipulator(manipulate2.MalwareManipulator):
     pass
 
-
-if __name__ == "__main__":
+if __name__ == "__main__":     
     downloader = filebrowser.FileBrowserClient(hosts=[filebrowser.HostInfo("10.112.255.77", "8081", "admin", "daxiahyh")])
     fetcher = fetch.Fetcher(indexes=["../all.benign.csv", "../all.malware.csv"], downloader = downloader)
-    bytez = fetcher.fetch("fa92ee9f25abbcc1b32d7567f8d68c34ce676fc853bca35a310d329107024535")
+    bytez = fetcher.fetch("47d8aed6a727a52b967f038e2c7234782e6f7ddbbaf2de1c2cbc8cb03fb995d4")
     print(hex(len(bytez)))
-    manipulator = MyManipulator(bytez)
-    print(hex(len(manipulator.overlay_append())))
+    import ipdb; ipdb.set_trace()    
+    manipulator = MyManipulator(bytez)    
     bytez = manipulator.imports_append()
     binary = lief.PE.parse(bytez)
     builder = lief.PE.Builder(binary)
     builder.build()
     builder.write("../output/test.exe")
-
-
 
